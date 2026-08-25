b3f5ff31bf858300b51af6d5c691e65f5611f1d7
ermionikwn <45976725+ermionikwn@users.noreply.github.com>
2026-08-25 17:42:42 +0300

    Create analysis.py

---
+++ analysis.py
@@ -0,0 +1,11 @@
+import pandas as pd
+
+data = {
+    "income": [25000, 40000, 55000, 30000, 70000],
+    "loan_amount": [10000, 15000, 20000, 12000, 25000],
+    "default": [1, 0, 0, 1, 0]
+}
+
+df = pd.DataFrame(data)
+
+print(df)
